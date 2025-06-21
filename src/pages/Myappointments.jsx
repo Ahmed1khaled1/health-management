1c643dcb5a100d2b3cfca5f2ad5b558c48851159
Ahmed-khaled <ahmedkhaledg49@gmail.com>
2025-06-21 17:22:32 +0300

    111

--- src/pages/Myappointments.jsx
+++ src/pages/Myappointments.jsx
@@ -5,20 +5,20 @@ import { AppContext } from "../context/AppContext";
 const Myappointments = () => {
   const { doctors } = useContext(AppContext);
   return (
-    <div>
+    <>
       <Hero titlePage="Patient Appointments" />
       <div className="w-full  flex justify-center items-center p-7 md:px-10 lg:px-20">
         <div className="w-full md:max-w-11/12 mx-auto flex flex-col items-center justify-center">
           {doctors.slice(0, 3).map((doctor) => (
             <div
               key={doctor._id}
-              className="w-full flex flex-col lg:flex-row lg:justify-between items-center gap-5 border-y border-gray-300 p-3"
+              className="w-full flex flex-col lg:flex-row lg:justify-between items-center gap-5 border-y border-gray-300 py-3"
             >
               <div className=" flex gap-7">
                 <img
                   src={doctor.image}
                   alt="doctor_image"
-                  className="w-40 bg-cyan-500/10 rounded-md"
+                  className="w-40 object-cover  bg-cyan-500/10 rounded-md"
                 />
                 <div className="">
                   <p className="font-semibold"> {doctor.name} </p>
@@ -33,18 +33,18 @@ const Myappointments = () => {
                 </div>
               </div>
               <div className="flex lg:flex-col gap-5 ">
-                <button className="py-2 border border-gray-300 rounded-md w-45 bg-primary text-white cursor-pointer">
+                <button className="py-2 border border-gray-300 rounded-md w-40 bg-primary text-white cursor-pointer">
                   Pay Online
                 </button>
-                <button className="py-2 border border-gray-300 rounded-md w-45 bg-second text-white cursor-pointer">
-                  Cancel Appointment{" "}
+                <button className="py-2 border border-gray-300 rounded-md w-40 bg-second text-white cursor-pointer">
+                  Cancel 
                 </button>
               </div>
             </div>
           ))}
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
